8189c93cb7bd3f838eeba89ea5d6eaae78668838
Maja Binkiewicz <1majabinkiewicz1@gmail.com>
2024-10-27 14:23:02 +0100

    Model User i app w INSTALLED_APPS

--- warsztat/settings.py
+++ warsztat/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'warsztat_app.apps.WarsztatAppConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
